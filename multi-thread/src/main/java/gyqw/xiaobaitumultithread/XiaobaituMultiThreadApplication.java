f9a268e3ef774273fa150f1b875fe600d110c628
guye <guye@geexfinance.com>
2018-06-11 23:22:49 +0800

    Add: multi thread

---
+++ multi-thread/src/main/java/gyqw/xiaobaitumultithread/XiaobaituMultiThreadApplication.java
@@ -0,0 +1,12 @@
+package gyqw.xiaobaitumultithread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaobaituMultiThreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaobaituMultiThreadApplication.class, args);
+	}
+}
