88dcd0cfa1a0666930f1d39f5d4c2385b4c61786
guye <guye@geexfinance.cn>
2018-06-14 10:05:49 +0800

    Add: edit distance

---
+++ edit-distance/src/main/java/gyqw/xiaobaitu/distance/XiaobaituEditDistanceApplication.java
@@ -0,0 +1,15 @@
+package gyqw.xiaobaitu.distance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author fred
+ */
+@ComponentScan
+public class XiaobaituEditDistanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaobaituEditDistanceApplication.class, args);
+    }
+}
