f923325e00152f78397cc439cf82baf9da32c5df
guye <guye@geexfinance.com>
2018-06-21 22:48:55 +0800

    Update: 二叉树添加与查找

--- binary-tree/src/main/java/gyqw/xiaobaitu/binary/tree/TreeMain.java
+++ binary-tree/src/main/java/gyqw/xiaobaitu/binary/tree/TreeMain.java
@@ -7,10 +7,77 @@ import gyqw.xiaobaitu.binary.tree.model.Node;
  * @date 2018/06/21 11:11
  */
 public class TreeMain {
+    private Node root;
+
     public static void main(String[] args) {
+        TreeMain treeMain = new TreeMain();
+        treeMain.insert(1, 1d);
+        treeMain.insert(2, 2d);
+
+        Node node0 = treeMain.find(2);
+        if (node0 != null) {
+            node0.showNode();
+        }
+        Node node1 = treeMain.find(1);
+        if (node1 != null) {
+            node1.showNode();
+        }
+
+        System.out.println("end");
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    private void insert(int iData, double dData) {
+        // 创建一个node节点
         Node node = new Node();
-        node.setiData(1);
-        node.setdData(10d);
-        node.showNode();
+        node.setiData(iData);
+        node.setdData(dData);
+
+        // 判断root是否为空
+        if (root == null) {
+            root = node;
+        } else {
+            Node current = root;
+            Node parent;
+
+            while (true) {
+                parent = current;
+
+                if (iData < current.getiData()) {
+                    current = current.getLeftNode();
+                    if (current == null) {
+                        parent.setLeftNode(node);
+                        return;
+                    }
+                } else {
+                    current = current.getRightNode();
+                    if (current == null) {
+                        parent.setRightNode(node);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    private Node find(int key) {
+        Node current = root;
+
+        while (current.getiData() != key) {
+            if (key < current.getiData()) {
+                current = current.getLeftNode();
+            } else {
+                current = current.getRightNode();
+            }
+
+            if (current == null) {
+                return null;
+            }
+        }
+
+        return current;
     }
 }
