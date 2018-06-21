2c775a6800d39c56e859f156e49ea5e9092e7d05
guye <guye@geexfinance.cn>
2018-06-21 13:50:49 +0800

    Add: 添加二叉树

---
+++ binary-tree/src/main/java/gyqw/xiaobaitu/binary/tree/TreeMain.java
@@ -0,0 +1,16 @@
+package gyqw.xiaobaitu.binary.tree;
+
+import gyqw.xiaobaitu.binary.tree.model.Node;
+
+/**
+ * @author fred
+ * @date 2018/06/21 11:11
+ */
+public class TreeMain {
+    public static void main(String[] args) {
+        Node node = new Node();
+        node.setiData(1);
+        node.setdData(10d);
+        node.showNode();
+    }
+}
