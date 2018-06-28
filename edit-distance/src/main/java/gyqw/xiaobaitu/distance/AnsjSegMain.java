4f0aaa74af3d16ca9fda827dbf7efa7eac79abbb
guye <guye@geexfinance.cn>
2018-06-28 17:23:17 +0800

    Add: 排序

--- edit-distance/src/main/java/gyqw/xiaobaitu/distance/AnsjSegMain.java
+++ edit-distance/src/main/java/gyqw/xiaobaitu/distance/AnsjSegMain.java
@@ -1,6 +1,9 @@
 package gyqw.xiaobaitu.distance;
 
+import org.ansj.domain.Term;
 import org.ansj.splitWord.analysis.ToAnalysis;
+
+import java.util.Iterator;
 
 /**
  * @author fred
@@ -12,7 +15,10 @@ public class AnsjSegMain {
         System.out.println(ToAnalysis.parse(str));
         String address = "湖北省 武汉市 东西湖区革新大道四明路物流园c5门";
         System.out.println(ToAnalysis.parse(address));
-        String company = "即科金融信息服务(上海)有限公司";
-        System.out.println(ToAnalysis.parse(company));
+        String company = "北京花旺在线商贸有限公司";
+        Iterator<Term> result = ToAnalysis.parse(company).iterator();
+        while (result.hasNext()) {
+            System.out.println(result.next().getSynonyms());
+        }
     }
 }
