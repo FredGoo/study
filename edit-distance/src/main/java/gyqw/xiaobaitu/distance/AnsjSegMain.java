e346ed54b40ceb66ec389a31b753a7123d5d858c
guye <guye@geexfinance.cn>
2018-06-29 17:30:36 +0800

    Update: format

--- edit-distance/src/main/java/gyqw/xiaobaitu/distance/AnsjSegMain.java
+++ edit-distance/src/main/java/gyqw/xiaobaitu/distance/AnsjSegMain.java
@@ -11,14 +11,12 @@ import java.util.Iterator;
  */
 public class AnsjSegMain {
     public static void main(String[] args) {
-        String str = "欢迎使用ansj_seg,(ansj中文分词)在这里如果你遇到什么问题都可以联系我.我一定尽我所能.帮助大家.ansj_seg更快,更准,更自由!";
-        System.out.println(ToAnalysis.parse(str));
         String address = "湖北省 武汉市 东西湖区革新大道四明路物流园c5门";
         System.out.println(ToAnalysis.parse(address));
         String company = "北京花旺在线商贸有限公司";
         Iterator<Term> result = ToAnalysis.parse(company).iterator();
         while (result.hasNext()) {
-            System.out.println(result.next().getSynonyms());
+            System.out.println(result.next().getName());
         }
     }
 }
--- sort/src/main/java/gyqw/xiaobaitu/sort/InsertionSortMain.java
+++ sort/src/main/java/gyqw/xiaobaitu/sort/InsertionSortMain.java
@@ -16,7 +16,7 @@ public class InsertionSortMain {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j > 0; j--) {
                 if (arr[j] >= arr[j - 1]) {
-                    
+
                 }
             }
         }
