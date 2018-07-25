fc84195914f6ea2c0ea431ba7f51ab2bfeda9296
guye <guye@geexfinance.cn>
2018-07-25 17:06:15 +0800

    Update: string to drl

--- drools/src/main/java/gyqw/xiaobaitu/drools/DroolsMain.java
+++ drools/src/main/java/gyqw/xiaobaitu/drools/DroolsMain.java
@@ -2,8 +2,11 @@ package gyqw.xiaobaitu.drools;
 
 import gyqw.xiaobaitu.drools.model.AppInfoModel;
 import org.kie.api.KieServices;
+import org.kie.api.builder.Results;
+import org.kie.api.io.ResourceType;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.internal.utils.KieHelper;
 
 /**
  * @author fred
@@ -11,19 +14,55 @@ import org.kie.api.runtime.KieSession;
  */
 public class DroolsMain {
     public static void main(String[] args) {
+        DroolsMain droolsMain = new DroolsMain();
+        KieSession kieSession = droolsMain.fromString();
+
         try {
             AppInfoModel appInfoModel = new AppInfoModel();
             appInfoModel.setAge(99);
 
-            KieServices kss = KieServices.Factory.get();
-            KieContainer kc = kss.getKieClasspathContainer();
-
-            KieSession ks = kc.newKieSession();
-            ks.insert(appInfoModel);
-            int firedRules = ks.fireAllRules();
+            kieSession.insert(appInfoModel);
+            int firedRules = kieSession.fireAllRules();
             System.out.println(firedRules);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+    }
+
+    private KieSession fromString() {
+        String drl = "package gyqw.xiaobaitu.drools;\n" +
+                "\n" +
+                "import gyqw.xiaobaitu.drools.model.AppInfoModel;\n" +
+                "\n" +
+                "        rule \"old-lady\"\n" +
+                "            salience 1\n" +
+                "            when\n" +
+                "                $p : AppInfoModel(age > 40);\n" +
+                "            then\n" +
+                "                $p.setHouseHoldAddressCity(\"少年\");\n" +
+                "                retract($p);\n" +
+                "                System.out.println($p.getStoreAddressCity()+\" \"+$p.getHouseHoldAddressCity());\n" +
+                "        end";
+        KieHelper kieHelper = new KieHelper();
+        kieHelper.addContent(drl, ResourceType.DRL);
+
+        Results results = kieHelper.verify();
+        System.out.println(results);
+
+        return kieHelper.build().newKieSession();
+    }
+
+    private KieSession fromResource() {
+        try {
+            KieServices kss = KieServices.Factory.get();
+            KieContainer kc = kss.getKieClasspathContainer();
+
+            return kc.newKieSession();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
