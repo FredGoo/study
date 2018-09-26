e74f8ec039d4dfc36110252ebf12c714cbce995a
guye <guye@geexfinance.com>
2018-09-26 17:37:21 +0800

    Update: cpp test

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a = 5;
+    int b = 1;
+    int result;
+    string hello = "Hello, World!";
+    string program = "I'm a C++ program";
+
+    result = a + b;
+    cout << result << result << result << endl;
+
+    result = a % 19;
+    cout << result << endl;
+
+    result = a > b ? a : b;
+    cout << result << endl;
+
+    cout << hello << endl;
+    cout << program << endl;
+
+
+    cout << hello.size() << endl;
+
     return 0;
 }
