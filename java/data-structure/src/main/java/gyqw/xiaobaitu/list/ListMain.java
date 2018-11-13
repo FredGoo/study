91cc76c897b04ecc0f671a0460a74652d884dd8a
guye <guye@geexfinance.com>
2018-11-13 17:00:14 +0800

    添加list

--- java/data-structure/src/main/java/gyqw/xiaobaitu/list/ListMain.java
+++ java/data-structure/src/main/java/gyqw/xiaobaitu/list/ListMain.java
@@ -1,10 +1,11 @@
-package gyqw.xiaobaitu.queue;
+package gyqw.xiaobaitu.list;
 
 /**
  * @author fred
- * 2018-11-07 10:18 AM
+ * 2018-11-13 4:59 PM
  */
-public class QueueMain {
+public class ListMain {
     public static void main(String[] args) {
+
     }
 }
