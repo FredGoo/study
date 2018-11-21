d7bbe0ec52179690e073ebb0559bf9516936bebe
guye <guye@geexfinance.com>
2018-11-21 16:22:24 +0800

    添加dgraph

--- java/data-structure/src/main/java/gyqw/xiaobaitu/list/ListMain.java
+++ java/data-structure/src/main/java/gyqw/xiaobaitu/list/ListMain.java
@@ -1,5 +1,7 @@
 package gyqw.xiaobaitu.list;
 
+import java.util.*;
+
 /**
  * @author fred
  * 2018-11-13 4:59 PM
@@ -7,5 +9,33 @@ package gyqw.xiaobaitu.list;
 public class ListMain {
     public static void main(String[] args) {
 
+    }
+
+    public void desc() {
+        Collection collection = new AbstractCollection() {
+            @Override
+            public Iterator iterator() {
+                return null;
+            }
+
+            @Override
+            public int size() {
+                return 0;
+            }
+        };
+
+        List list = new AbstractList() {
+            @Override
+            public Object get(int i) {
+                return null;
+            }
+
+            @Override
+            public int size() {
+                return 0;
+            }
+        };
+
+        List list1 = new ArrayList();
     }
 }
---
+++ java/dgraph/src/main/java/gyqw/xiaobaitu/dgraph/DgraphApplication.java
@@ -0,0 +1,15 @@
+package gyqw.xiaobaitu.dgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fred
+ * 2018-11-21 4:17 PM
+ */
+@SpringBootApplication
+public class DgraphApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DgraphApplication.class, args);
+    }
+}
