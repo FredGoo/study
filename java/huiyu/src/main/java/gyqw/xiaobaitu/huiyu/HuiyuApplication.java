220b717db7c347475fdc9402394ac50a01d23845
guye <guye@geexfinance.com>
2019-03-05 18:24:58 +0800

    huiyu应用cli

--- java/huiyu/src/main/java/gyqw/xiaobaitu/huiyu/HuiyuApplication.java
+++ java/huiyu/src/main/java/gyqw/xiaobaitu/huiyu/HuiyuApplication.java
@@ -1,17 +1,19 @@
 package gyqw.xiaobaitu.huiyu;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
+import gyqw.xiaobaitu.huiyu.dto.BaseResult;
+import gyqw.xiaobaitu.huiyu.service.AiRequest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * @author fred
  * 2018-10-31 5:47 PM
  */
-@SpringBootApplication
 public class HuiyuApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HuiyuApplication.class, args);
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext("huiyu");
+
+        AiRequest aiRequest = applicationContext.getAutowireCapableBeanFactory().getBean(AiRequest.class);
+        BaseResult baseResult = aiRequest.queryList("190213001514", 1, 10);
     }
 }
