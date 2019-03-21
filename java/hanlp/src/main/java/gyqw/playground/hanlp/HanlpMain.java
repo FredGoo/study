b2a63b62e8189c788953bc1fd481dc3e9fc1a9f8
guye <guye@geexfinance.com>
2019-03-21 18:23:25 +0800

    feat(hanlp): 短信分类

--- java/hanlp/src/main/java/gyqw/playground/hanlp/HanlpMain.java
+++ java/hanlp/src/main/java/gyqw/playground/hanlp/HanlpMain.java
@@ -1,8 +1,15 @@
 package gyqw.playground.hanlp;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.hankcs.hanlp.mining.cluster.ClusterAnalyzer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author fred
@@ -13,7 +20,7 @@ public class HanlpMain {
 
     public static void main(String[] args) {
         HanlpMain hanlpMain = new HanlpMain();
-        hanlpMain.manSound();
+        hanlpMain.sms();
     }
 
     private void manSound() {
@@ -26,5 +33,28 @@ public class HanlpMain {
         analyzer.addDocument("马六", "古典, 古典, 古典, 古典, 古典, 古典, 古典, 古典, 摇滚");
 
         logger.info(analyzer.repeatedBisection(1.0).toString());
+    }
+
+    private void sms() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("data/test/sms/customer.json"));
+            String s;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((s = br.readLine()) != null) {
+                stringBuilder.append(s);
+            }
+            logger.info("原始json: " + stringBuilder.toString());
+
+            ClusterAnalyzer<String> analyzer = new ClusterAnalyzer<>();
+            Gson gson = new Gson();
+            List<Map<String, Object>> list = gson.fromJson(stringBuilder.toString(), new TypeToken<List<Map<String, Object>>>() {
+            }.getType());
+            for (Map<String, Object> map : list) {
+                analyzer.addDocument(map.get("ID").toString(), map.get("SMS_CONTENT").toString());
+            }
+            logger.info(analyzer.repeatedBisection(1.0).toString());
+        } catch (Exception e) {
+            logger.error("sms error", e);
+        }
     }
 }
